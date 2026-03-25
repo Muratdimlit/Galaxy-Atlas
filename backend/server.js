3c6387ac60f5138a1a83951881c84b6171c40df6
muweaf <muratdimlit80@gmail.com>
2026-03-25 14:09:52 +0300

    5,6,7,8 ve 9.gereksinimler tamamlandı

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
+const spaceObjectRoutes = require("./routes/spaceObjectRoutes");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", authRoutes);
+app.use("/space-objects", spaceObjectRoutes);
 
 const PORT = process.env.PORT || 5000;
 
